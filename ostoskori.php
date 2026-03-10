e1096fc141c2a8ea70eb0c188bbfb32169902aeb
Heini Syrjälä <hempula88@hotmail.com>
2026-03-10 19:34:26 +0200

    varmuuskopio ennen viimeisiä muutoksia

---
+++ ostoskori.php
@@ -0,0 +1,24 @@
+<?php
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+try{
+$yhteys = mysqli_connect("localhost", "trtkm25b3_5", "LoOcNj7g", "trtkm25b3_5");
+}
+catch(Exception $e){
+    header("Location:../../html/yhteysvirhe.html");
+    exit;
+}
+
+$tulos = mysqli_query($yhteys, "select * from ostoskori");
+while ($rivi=mysqli_fetch_object($tulos)) {
+    $tuote = new class{};
+    $tuote->id=$rivi->id;
+    $tuote->tuotenro=$rivi->tuotenro;
+    $tuote->tuotenimi=$rivi->tuotenimi;
+    $tuote->hinta=$rivi->hinta;
+    $tuote->maara=$rivi->maara;
+    $tuotteet[]=$tuote;
+}
+mysqli_close($yhteys);
+print json_encode($tuotteet);
+exit;
+?>
